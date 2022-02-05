60802aa3881add69873c7e288c52e77b45e97a93
daviddetorres <maellyssa@gmail.com>
2022-02-05 23:23:18 +0100

    Working on getting data from github repositories
    Signed-off-by: daviddetorres <maellyssa@gmail.com>

---
+++ supply-chain-score/model/repo.py
@@ -0,0 +1,71 @@
+
+
+class Repo:
+    """
+    Repo class.
+    """
+
+    def __init__(self, url):
+        """
+        Initialize Repo class.
+        """
+        self.url = url
+        self.owner = self.get_owner()
+        self.name = self.get_name()
+        self.contributors = self.get_contributors()
+        self.forks = self.get_forks()
+        self.releases = self.get_releases()
+        self.stars = self.get_stars()
+        self.issues = self.get_issues()
+        self.commits = self.get_commits()
+
+    def __str__(self):
+        """
+        Return string representation of Repo object.
+        """
+        return "Repo: {0}".format(self.name)
+      
+    def get_contributors(self):
+        """
+        Return number of contributors for repo.
+        """
+        pass
+
+    def get_forks(self):
+        """
+        Return number of forks for repo.
+        """
+        pass
+
+    def get_releases(self):
+        """
+        Return number of releases for repo.
+        """
+        pass
+
+    def get_stars(self):
+        """
+        Return number of stars for repo.
+        """
+        pass
+    
+
+    def get_issues(self):
+        """
+        Return number of issues for repo.
+        """
+        pass
+
+
+    def get_commits(self):
+        """
+        Return number of commits for repo.
+        """
+        pass
+    
+    def get_score(self):
+        """
+        Return score for repo.
+        """
+        pass
+    
